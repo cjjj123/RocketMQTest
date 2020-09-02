da2a97319de9e0777aca39b88aaa31bc924726aa
dengjunyu668@163.com <dengjunyu668@163.com>
2020-09-02 17:44:22 +0800

    RocketMQ测试

---
+++ rocketmqtest/src/main/java/com/cjj/RocketmqtestApplication.java
@@ -0,0 +1,13 @@
+package com.cjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqtestApplication.class, args);
+	}
+
+}
